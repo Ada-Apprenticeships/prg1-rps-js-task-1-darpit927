e92e9a4a5f4ec423a6637ec400f708a7c7d80dc3
darpit927 <darpit.shah@ada.ac.uk>
2024-10-16 12:11:28 +0000

    rps code inputted

--- rps.js
+++ rps.js
@@ -1,6 +1,11 @@
 function rockPaperScissors(player1, player2) {
-
-  
+  if (player1===player2){
+    return "draw"
+  }else if ((player1==="rock"&&player2==="scissors")
+    ||(player1==="paper"&&player2==="rock")
+    ||(player1==="scissors"&&player2==="paper")){
+      return "player1 wins"
+    }else {return "player2 wins"}
    
 }
 
