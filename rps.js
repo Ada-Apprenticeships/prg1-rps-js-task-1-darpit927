cb2d021304d5aa1a5ec0fd5b67d29d2d16f24355
darpit927 <darpit.shah@ada.ac.uk>
2024-10-20 14:25:50 +0000

    Refactored

--- rps.js
+++ rps.js
@@ -1,20 +1,16 @@
-// The function returns the winner of Rock, Paper, Scissors game between 2 players 
 function rockPaperScissors(player1, player2) {
-  //Creates an object that includes arrays showing what wins against what
-  const winningPrecedence = {
+  // If both players choose the same, it's a draw
+  if (player1 === player2) return "draw";
+  // Object defining what each move beats
+  const winningOutcomes = {
     rock: ["scissors", "lizard"],
     paper: ["rock", "spock"],
     scissors: ["paper", "lizard"],
     lizard: ["spock", "paper"],
     spock: ["rock", "scissors"]
   };
-// Verifies conditions that returns the winner 
-  return player1 === player2
-  ? "draw"
-  // Returns player1 as the winner as long as player2's choice is included in player1's array in the object 
-  : winningPrecedence[player1].includes(player2)
-  ? "player1"
-  : "player2";
+  // Check if player1's move beats player2's move, otherwise player2 wins
+  return winningOutcomes[player1].includes(player2) ? "player1" : "player2";
 }
 // Leave this code here for the automated tests
 module.exports = {
